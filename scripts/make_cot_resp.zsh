870c31293173e792eb4615f309742d159045d30c
Jett <jettjaniak@gmail.com>
2025-01-13 17:50:27 +0000

    adapt make_cot_resp.zsh to OpenRouter

--- scripts/make_cot_resp.zsh
+++ scripts/make_cot_resp.zsh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 for file in d/questions/**/*.yaml; do
-    for model_id in G2 L1 L3 P Q0.5 Q1.5 Q3; do
+    for model_id in GF1.5 C3H; do
         # :t gets the tail (filename), :r removes extension
         dataset_id=${file:t:r}
         if [[ $dataset_id = *tests* ]]; then
             continue
         fi
-        if [[ $dataset_id = aircraft-speeds* || $dataset_id = boiling-points* ]]; then
+        if [[ $dataset_id = animals-speed* || $dataset_id = sea-depths* || $dataset_id = sound-speeds* || $dataset_id = train-speeds* ]]; then
             continue
         fi
-        ./scripts/gen_cots.py -d "$dataset_id" -m "$model_id" -n 10
+        ./scripts/gen_cots.py -d "$dataset_id" -m "$model_id" -n 10 --or 
     done
 done
