b9521defcef74c87a9ee4281f17207f362e49c57
Ivan Arcuschin <iarcuschin@gmail.com>
2025-01-14 00:21:17 +0000

    Use bash for make_cot_resp script

--- scripts/make_cot_resp.sh
+++ scripts/make_cot_resp.sh
@@ -1,8 +1,9 @@
-#!/bin/zsh
+#!/bin/bash
 for file in d/questions/**/*.yaml; do
     for model_id in GF1.5 C3H; do
         # :t gets the tail (filename), :r removes extension
-        dataset_id=${file:t:r}
+        dataset_id="${file##d/questions/*/}"
+        dataset_id="${dataset_id%.yaml}"
         if [[ $dataset_id = *tests* ]]; then
             continue
         fi
