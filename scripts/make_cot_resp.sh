c25771a1fe3df7d38f36729293fc232545fa3426
Ivan Arcuschin <iarcuschin@gmail.com>
2025-01-18 13:04:15 -0800

    Minor change to make_cot_resp.sh

--- scripts/make_cot_resp.sh
+++ scripts/make_cot_resp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-for file in d/questions/**/*.yaml; do
-    for model_id in C3.5H; do
+for model_id in C3.5S; do
+    echo "Processing model $model_id"
+
+    for file in d/questions/**/*.yaml; do
         dataset_id="${file##d/questions/*/}"
         dataset_id="${dataset_id%.yaml}"
         if [[ $dataset_id = *tests* ]]; then
@@ -9,6 +11,8 @@ for file in d/questions/**/*.yaml; do
         if [[ $dataset_id = animals-speed* || $dataset_id = sea-depths* || $dataset_id = sound-speeds* || $dataset_id = train-speeds* ]]; then
             continue
         fi
-        ./scripts/gen_cots.py -d "$dataset_id" -m "$model_id" -n 10 --or 
+
+        echo "Processing dataset $dataset_id"
+        ./scripts/gen_cots.py -d "$dataset_id" -m "$model_id" -n 10 --or --append
     done
 done
