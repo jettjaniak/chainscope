23da6ca5db0415aff633a4747a7c76df15f8fc9d
Ivan Arcuschin <iarcuschin@gmail.com>
2025-04-17 18:35:51 +0000

    Minor change

--- scripts/iphr/make_cot_resp.sh
+++ scripts/iphr/make_cot_resp.sh
@@ -4,8 +4,8 @@ for model_id in C3.5H; do
     for file in d/questions/**/*.yaml; do
         dataset_id="${file##d/questions/*/}"
         dataset_id="${dataset_id%.yaml}"
-        if [[ $dataset_id = wm* ]]; then
-            echo "Processing wm dataset $dataset_id"
+        if [[ $dataset_id = *non-ambiguous-obscure-or-close-call ]]; then
+            echo "Processing dataset $dataset_id"
             ./scripts/iphr/gen_cots.py submit -d "$dataset_id" -m "$model_id" -n 10 -i instr-wm --api ant-batch
         fi
     done
