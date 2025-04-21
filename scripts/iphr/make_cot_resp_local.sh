25c7e46cd38bf731f793b1894e49b153736d848a
robertzk <technoguyrob@gmail.com>
2025-04-21 23:47:25 +0000

    Add local iphr script

---
+++ scripts/iphr/make_cot_resp_local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for model_id in QwQ; do
+    echo "Processing model $model_id"
+    for file in d/questions/**/*.yaml; do
+        dataset_id="${file##d/questions/*/}"
+        dataset_id="${dataset_id%.yaml}"
+        if [[ $dataset_id = wm* ]]; then
+            echo "Processing wm dataset $dataset_id"
+	    if [[ $dataset_id != "wm-book-length_gt_NO_1_6fda02e3" ]]; then
+		   poetry run ./scripts/iphr/gen_cots.py submit -d "$dataset_id" -m "$model_id" -n 1 -i instr-wm --api local-ts
+	    fi
+        fi
+    done
+done
