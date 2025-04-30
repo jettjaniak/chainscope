d3a1e2bb91cadfeef94fc81aac8f87d72bf3570d
robertzk <technoguyrob@gmail.com>
2025-04-30 22:11:27 +0000

    Debugging changes.

--- scripts/iphr/make_cot_resp_local.sh
+++ scripts/iphr/make_cot_resp_local.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 for model_id in QwQ; do
     echo "Processing model $model_id"
-    for file in $(ls -r d/questions/**/*.yaml); do
+    for file in $(ls d/questions/**/*.yaml); do
         dataset_id="${file##d/questions/*/}"
         dataset_id="${dataset_id%.yaml}"
-        if [[ $dataset_id = wm* ]]; then
+        #if [[ $dataset_id = wm* ]]; then
             echo "Processing wm dataset $dataset_id"
             if [[ $dataset_id != "wm-book-length_gt_NO_1_6fda02e3" ]]; then
                 poetry run ./scripts/iphr/gen_cots.py submit -d "$dataset_id" -m "$model_id" -n 1 -i instr-wm --api local-tl
                 exit
             fi
-        fi
+       # fi
     done
 done
