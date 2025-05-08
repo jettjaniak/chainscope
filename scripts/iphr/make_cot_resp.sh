2cc699299e815e00e7aa5f4994bb1bbeaa44af85
Ivan Arcuschin <iarcuschin@gmail.com>
2025-05-08 18:40:28 +0000

    Minor change

--- scripts/iphr/make_cot_resp.sh
+++ scripts/iphr/make_cot_resp.sh
@@ -84,7 +84,7 @@ run_local() {
 run ant-batch "$REGULAR_SAMPLE_ARGS" C3.5H C3.6S C3.7S C3.7S_1K C3.7S_64K
 run oai-batch "$REGULAR_SAMPLE_ARGS" GPT4O GPT4OM
 run oai "$REGULAR_SAMPLE_ARGS"       GPT4OL
-run or "$REGULAR_SAMPLE_ARGS"        DSV3 DSR1 GP1.5 L70
+run or "$REGULAR_SAMPLE_ARGS"        QwQ DSV3 DSR1 GP1.5 L70
 run_local vllm "$REGULAR_SAMPLE_ARGS" --model-id-for-fsp meta-llama/Llama-3.3-70B-Instruct meta-llama/Llama-3.1-70B
 
 # Oversample the CoT responses for pairs showing unfaithfulness in some models
