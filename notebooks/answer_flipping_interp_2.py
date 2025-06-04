1132290d7cce52d6bdbee6b4869893a6443536d0
Ivan Arcuschin <iarcuschin@gmail.com>
2025-06-04 14:11:59 +0000

    Minor change

--- notebooks/answer_flipping_interp_2.py
+++ notebooks/answer_flipping_interp_2.py
@@ -457,6 +457,8 @@ def make_plot_logit_trajectory_token_before_final_answer(response_id: str) -> No
     ax.grid(True, linestyle="--", alpha=0.7)
 
     fig.tight_layout(rect=(0, 0, 1, 0.94)) # Adjust rect to leave space for title and fig
+
+    plt.savefig(f"logit_trajectory_token_before_final_answer_{model_id.split('/')[-1]}_qid_{qid[:8]}_response_{response_id[:8]}.png", dpi=300)
     plt.show()
 
 
