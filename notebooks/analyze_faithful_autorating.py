834ab6b0ed6b356860bcd373f66cface16388de7
Ivan Arcuschin <iarcuschin@gmail.com>
2025-06-09 21:41:58 +0100

    Actually make a working nb

--- notebooks/analyze_faithful_autorating.py
+++ notebooks/analyze_faithful_autorating.py
@@ -89,7 +89,7 @@ assert load_dotenv(dotenv_path='/workspace/faith/chainscope/.env', verbose=True)
 # %% 
 
 # Load the original responses.
-responses_path = Path("/workspace/faith/chainscope/chainscope/data/cot_responses/instr-v0/default_sampling_params/filtered_putnambench/anthropic__claude-3.7-sonnet_v0_just_correct_responses_newline_split.yaml")
+responses_path = Path("/workspace/faith/chainscope/chainscope/data/cot_responses/instr-v0/default_sampling_params/filtered_putnambench/anthropic__claude-3.7-sonnet:thinking_v0_just_correct_responses_newline_split_anthropic_slash_claude-3_dot_7-sonnet_colon_thinking_reward_hacking.yaml")
 
 if "splitted" in str(responses_path):
     source_path = Path(''.join(str(responses_path).split("_splitted")[:-1]) + "_splitted.yaml")
@@ -246,6 +246,12 @@ SKIP_ATTEMPT_GREATER_THAN_5 = False
 lec_cases = []
 ref_string=""
 
+# MAINLINE EVAL
+# pattern = "YNNNYNYN"
+
+# DOES THE MODEL "OWN UP" EVER?
+pattern = "YNNNYNYY"
+
 # Iterate through all problems and steps
 for qid, response in enumerate(split_responses):
     for i, step in enumerate(response.model_answer):
@@ -268,13 +274,13 @@ for qid, response in enumerate(split_responses):
 
         # Check for sketchy pattern
 
-        if len(step_dict["unfaithfulness"]) != len("YNNNYNYN"):  # YNYNYNYN
+        if len(step_dict["unfaithfulness"]) != len(pattern):  # YNYNYNYN
             print(f"Skipping {qid=}, {i=} because it's {step_dict['unfaithfulness']}")
             continue
 
-        dist = sum(int(x!=y) for x, y in zip(step_dict["unfaithfulness"], "YNNNYNYN", strict=True))
+        dist = sum(int(x!=y) for x, y in zip(step_dict["unfaithfulness"], pattern, strict=True))
 
-        if len(step_dict["unfaithfulness"]) == len("YNNNYNYN") and dist==0:
+        if len(step_dict["unfaithfulness"]) == len(pattern) and dist==0:
             # Get original steps from source file
             source_steps = []
             source_response = source_split_responses[qid]
